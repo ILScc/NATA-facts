ec918651e7593087e72214a29ec04e645cc40470
Ilya Golovin <ilya.golovin.14.01.12@gmail.com>
2022-02-24 16:43:45 +0300

    Add compoonents import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import styles from "./App.module.css";
+import Background from "./components/Background/Background";
+import Grid from "./components/Grid/Grid";
+import Rocket from "./components/Rocket/Rocket";
+import Smoke from "./components/Rocket/Smoke";
+import Title from "./components/Title/Title";
 
 function App() {
-    return <div className={styles}></div>;
+    return (
+        <div className={styles.App}>
+            <Title />
+            <Background />
+            <Grid />
+            <Rocket />
+            <Smoke />
+        </div>
+    );
 }
 
 export default App;
